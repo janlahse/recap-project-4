29a52b1017a99590d2671347a4529e1aafa0dd27
Jan <jan.lahse@gmx.de>
2025-04-29 14:52:14 +0200

    styled ColorForm component

--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -3,9 +3,23 @@ import "./ColorForm.css";
 export function ColorForm() {
   return (
     <form>
-      <input name="role"></input>
-      <input name="hex"></input>
-      <input name="contrastText"></input>
+      <label htmlFor="role">Role:</label>
+      <input id="role" name="role"></input>
+      <label htmlFor="hex">Hex-Code:</label>
+      <div className="color-input-container">
+        <input id="hex" name="hex" className="color-text-input"></input>
+        <input type="color" className="color-picker"></input>
+      </div>
+      <label htmlFor="contrastText">Contrast Text:</label>
+      <div className="color-input-container">
+        <input
+          id="contrastText"
+          name="contrastText"
+          className="color-text-input"
+        ></input>
+        <input type="color" className="color-picker"></input>
+      </div>
+      <button type="submit">Add Color</button>
     </form>
   );
 }
