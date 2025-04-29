e5c7fb18855b8ad8ddcc5ea6ef6c83ed5f60057b
Jan <jan.lahse@gmx.de>
2025-04-29 12:51:17 +0200

    created ColorForm component, added inputs including css

---
+++ src/Components/ColorForm/ColorForm.jsx
@@ -0,0 +1,11 @@
+import "./ColorForm.css";
+
+export function ColorForm() {
+  return (
+    <form>
+      <input name="role"></input>
+      <input name="hex"></input>
+      <input name="contrastText"></input>
+    </form>
+  );
+}
